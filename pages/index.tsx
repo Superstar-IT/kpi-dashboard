c30d622bbc6c468ccac69cd4682025466634d69e
DESKTOP-KVMRJR6\coold <dima10516@gmail.com>
2023-07-04 18:03:48 -0400

    remove unnecessary sections from dashboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,15 +31,6 @@ function Dashboard() {
           <Grid item xs={12}>
             <RecentKPIs />
           </Grid>
-          <Grid item lg={8} xs={12}>
-            <Wallets />
-          </Grid>
-          <Grid item lg={4} xs={12}>
-            <AccountSecurity />
-          </Grid>
-          <Grid item xs={12}>
-            <WatchList />
-          </Grid>
         </Grid>
       </Container>
     </>
